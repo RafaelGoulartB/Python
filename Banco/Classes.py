6223de00cf805e5fa4dfb9180c0369469edd4121
Rafael <rafagoulartb@gmail.com>
2018-05-31 08:50:46 -0300

    Banco

---
+++ Banco/Classes.py
@@ -0,0 +1,64 @@
+class Conta(object):
+	def __init__(self, no, num, tp, sd):
+		#Atributos
+		self.__nome = no
+		self.__numAcc = num
+		self.__tipo = tp 	#Tipos: CC, Conta Corrente. CP, Conta Corrente
+		self.__saldo = sd
+		self.__status = False
+
+	#Métodos Especiais
+	def __setNome(self, no):
+		self.__nome = no
+	def getNome(self):
+		return self.__nome
+
+	def __setNumAcc(self, num):
+		self.__numAcc = num
+	def getNumAcc(self):
+		return self.__numAcc
+
+	def __setTipo(self, tipo):
+		self.__tipo = tipo
+	def getTipo(self):
+		return self.__tipo
+
+	def __setSaldo(self, saldo):
+		self.__saldo = saldo
+	def getSaldo(self):
+		return self.__saldo
+
+	def __setStatus(self, status):
+		self.__status = status
+	def getStatus(self):
+		return self.__status
+
+	def getInfo(self):
+		print("Nome: "+self.getNome())
+		print("Num da Conta: "+ str(self.getNumAcc()))
+		print("Tipo: " + self.getTipo())
+		print("Saldo: "+ str(self.getSaldo()))
+		print("Status: "+ str(self.getStatus()))
+
+	#Métodos
+	def trasferir():
+		pass
+	def depositar(self, valor):
+		self.__setSaldo(self.getSaldo() + valor)
+
+	def sacar(self, valor):
+		if valor <= self.getSaldo():
+			self.__setSaldo(self.getSaldo() - valor)
+		else:
+			return False
+
+	def abrirAcc(self):
+		self.__setStatus = True
+		if self.getTipo() == 'CC':
+			self.__setSaldo(50)
+		else:
+			self.__setSaldo(100)
+	
+	def fecharAcc():
+		self.__setStatus = False
+		
